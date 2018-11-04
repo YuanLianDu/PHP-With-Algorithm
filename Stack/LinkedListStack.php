1a3c7b8bb1d636cb761ab944d931425a39a0975c
yuanliandu <yuanliandu@qq.com>
2018-11-04 15:50:11 +0800

    init linkedliststack

---
+++ Stack/LinkedListStack.php
@@ -0,0 +1,36 @@
+
+<?php
+// namespace Stack;
+
+
+class LinkedListStack extends Stack
+{
+    public function init(int $maxSize)
+    {
+    }
+
+    //  public function destroy(){}
+    public function clear()
+    {
+    }
+
+    public function isEmpty()
+    {
+    }
+
+    public function getTop()
+    {
+    }
+
+    public function push($item)
+    {
+    }
+
+    public function pop()
+    {
+    }
+
+    public function length()
+    {
+    }
+}
--- stack/Stack.php
+++ stack/Stack.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Stack;
 
 abstract class Stack {
