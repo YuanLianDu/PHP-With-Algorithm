bb0b8effc9d8604eeaaaf351adc112df31f0b64d
yuanliandu <yuanliandu@qq.com>
2018-11-04 15:44:33 +0800

    test array stack and modify to enable array stack

--- stack/Stack.php
+++ stack/Stack.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace stack;
+namespace Stack;
 
 abstract class Stack {
     
-abstract public function init();
+abstract public function init(int $maxSize);
 
-abstract public function destroy();
+// abstract public function destroy();
 abstract public function clear();
 abstract public function isEmpty();
 abstract public function getTop();
-abstract public function push();
+abstract public function push($item);
 abstract public function pop();
 abstract public function length();
 
--- stack/main.php
+++ stack/main.php
@@ -1,6 +1,20 @@
 <?php
+namespace Stack;
 
-$test = new SplFixedArray(5);
-$array[1] = 2;
-var_dump($array[0]); // NULL
-var_dump($array[1]);
+require_once '../vendor/autoload.php';
+
+
+$arrayStack = new ArrayStack();
+$arrayStack->init(10);
+for($i=0;$i<10;$i++) {
+    $arrayStack->push($i);
+}
+for($i=0;$i<5;$i++) {
+    print($arrayStack->pop());
+}
+var_dump($arrayStack->length());
+var_dump($arrayStack->getTop());
+var_dump($arrayStack->clear());
+var_dump($arrayStack->isEmpty());
+unset($arrayStack);//php 销毁对象使用unset即可
+var_dump($arrayStack);
